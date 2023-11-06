916b2dc74ffc66bb5c211456d287c9bee2c2f400
gruzdevnn <gruzdevnn@gmail.com>
2023-11-06 18:02:32 +0000

    HW7v10

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "cloud_id" {
   description = "Cloud"
- }
+}
 variable "folder_id" {
   description = "Folder"
- }
+}
 variable "zone" {
   description = "Zone"
-  default = "ru-central1-a"
- }
+  default     = "ru-central1-a"
+}
 variable "service_account_key_file" {
   description = "key.json"
- }
+}
 variable "access_key" {
   description = "key.json"
- }
+}
 variable "secret_key" {
   description = "key.json"
- }
+}
