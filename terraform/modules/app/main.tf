ba31d8fe79b6e54693c5afbbf4d960a0bbf85f8d
gruzdevnn <gruzdevnn@gmail.com>
2023-11-06 15:01:43 +0000

    HW6v4

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,18 +1,12 @@
 #terraform {
-  #required_providers {
-    #yandex = {
-      #source = "yandex-cloud/yandex"
-    #}
-  #}
-  #required_version = ">= 0.13"
-  #}
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 0.13"
+#  }
 
-  provider "yandex" {
-    service_account_key_file = var.service_account_key_file
-    cloud_id  = var.cloud_id
-    folder_id = var.folder_id
-    zone      = var.zone
-  }
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
 
@@ -32,7 +26,7 @@ resource "yandex_compute_instance" "app" {
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9b6novi7vdmhr9geb99"
+    subnet_id = var.subnet_id
     nat       = true
   }
 
--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,18 +1,11 @@
 #terraform {
- # required_providers {
-   # yandex = {
-    #  source = "yandex-cloud/yandex"
-   # }
-  #}
- # required_version = ">= 0.13"
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#  required_version = ">= 0.13"
 #}
-
-  provider "yandex" {
-    service_account_key_file = var.service_account_key_file
-    cloud_id  = var.cloud_id
-    folder_id = var.folder_id
-    zone      = var.zone
-  }
 
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
@@ -21,7 +14,7 @@ resource "yandex_compute_instance" "db" {
   }
 
   resources {
-    cores  = 2
+    cores  = 1
     memory = 2
   }
 
@@ -33,7 +26,7 @@ resource "yandex_compute_instance" "db" {
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9b6novi7vdmhr9geb99"
+    subnet_id = var.subnet_id
     nat       = true
   }
 
